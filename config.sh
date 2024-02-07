3523bda60cdbf316c5e5140599699e2bcf56b615
verisama <verisama@cucudada>
2024-02-07 12:22:03 +0100

    Update aliases

--- config.sh
+++ config.sh
@@ -2,9 +2,10 @@
 
 export ZSH_CONFIG="$HOME/.zsh"
 
-source $ZSH_CONFIG/aliases.sh
 source $ZSH_CONFIG/git.sh
-source $ZSH_CONFIG/prompt.sh
+source $ZSH_CONFIG/aliases.sh
 
 export EDITOR=code
 export GIT_EDITOR=vim
+
+alias aliases="$EDITOR $ZSH_CONFIG/aliases.sh"
